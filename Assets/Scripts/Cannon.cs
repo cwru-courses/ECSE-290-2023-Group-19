160496c7b7a59294a0bab0bca034beb0a2f65646
Cly0211 <chenliyi0211@126.com>
2023-04-09 17:44:04 -0400

    Write script: Cannon

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+    public Transform rotater;
+    public float turnSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, object.5f)
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectWithTag();
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearnestEnemy.transform;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotationAngle = Quaternion.Lerp(rotater.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        rotater.rotation = Quaternion.Euler(0f, rotationAngle.y, 0f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        OnDrawGizmosSelected().color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
