4dedb36817264db2ae361a8587e9a2f27ab51756
Cly0211 <chenliyi0211@126.com>
2023-04-09 15:17:35 -0400

    Write scripts: Waypoints, EnemyMovement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
