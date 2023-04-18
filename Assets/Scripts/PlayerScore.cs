320042644c6b5689791f079362b853e216eeeef2
Jing Wang <2805978454wj@gmail.com>
2023-04-18 00:48:45 -0400

    Instruction added

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerScore : MonoBehaviour
 {
@@ -23,6 +24,10 @@ public class PlayerScore : MonoBehaviour
     {
         gameDurationText.text = "Game Duration: " + gameDuration.ToString() + "s";
         PlayerHealthText.text = "Your Health: " + playerHealth + "hp";
+        if (playerHealth <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 
     void updateDuration()
