841953102ace929b522cb51186cb6ba24c464157
lsn19 <lsn19@case.edu>
2023-04-18 01:36:57 -0400

    animation for character

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -15,14 +15,14 @@ public class RelativeMovement : MonoBehaviour
 
     private PlayerCharacter playerCharacter;
 
-    public float moveSpeed = 0.0f;
+    public float moveSpeed = 10.0f;
     public float rotSpeed = 15.0f;
-    public float jumpSpeed = 25.0f;
+   // public float jumpSpeed = 25.0f;
     public float gravity = -9.8f;
     public float terminalVelocity = -20.0f;
-    public float minFall = -1.5f;
+   // public float minFall = -1.5f;
 
-    private float _vertSpeed;
+    //private float _vertSpeed;
     private ControllerColliderHit _contact;
 
     private CharacterController _charController;
@@ -31,7 +31,7 @@ public class RelativeMovement : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        _vertSpeed = minFall;
+        //_vertSpeed = minFall;
 
         _charController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
@@ -48,89 +48,33 @@ public class RelativeMovement : MonoBehaviour
         // x z movement transformed relative to target
         float horInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
-        if (playerCharacter.CanMove())
+
+        if (horInput != 0 || vertInput != 0)
         {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
 
-            moveSpeed = 10.0f;
-            if (horInput != 0 || vertInput != 0)
-            {
-                movement.x = horInput * moveSpeed;
-                movement.z = vertInput * moveSpeed;
-                movement = Vector3.ClampMagnitude(movement, moveSpeed);
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
 
-                Quaternion tmp = target.rotation;
-                target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
-                movement = target.TransformDirection(movement);
-                target.rotation = tmp;
-
-                // face movement direction
-                //transform.rotation = Quaternion.LookRotation(movement);
-                Quaternion direction = Quaternion.LookRotation(movement);
-                transform.rotation = Quaternion.Lerp(transform.rotation,
-                                                     direction, rotSpeed * Time.deltaTime);
-            }
-            _animator.SetFloat("Speed", movement.sqrMagnitude);
-
+            // face movement direction
+            //transform.rotation = Quaternion.LookRotation(movement);
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                                                    direction, rotSpeed * Time.deltaTime);
+        }
+        _animator.SetFloat("Speed", movement.sqrMagnitude);
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetBool("Kick", true);
         }
         else
         {
-            moveSpeed = 0.0f;
-        }
-        // raycast down to address steep slopes and dropoff edge
-        bool hitGround = false;
-        RaycastHit hit;
-        if (_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit) && playerCharacter.CanMove())
-        {
-            float check = (_charController.height + _charController.radius) / 1.9f;
-            hitGround = hit.distance <= check;  // to be sure check slightly beyond bottom of capsule
-        }
-
-        // y movement: possibly jump impulse up, always accel down
-        // could _charController.isGrounded instead, but then cannot workaround dropoff edge
-        if (hitGround && playerCharacter.CanMove())
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                _vertSpeed = jumpSpeed;
-            }
-            else
-            {
-                _vertSpeed = minFall;
-                _animator.SetBool("Jumping", false);
-            }
-        }
-        else
-        {
-            _vertSpeed += gravity * 5 * Time.deltaTime;
-            if (_vertSpeed < terminalVelocity)
-            {
-                _vertSpeed = terminalVelocity;
-            }
-            if (_contact != null)
-            {   // not right at level start
-                _animator.SetBool("Jumping", true);
-            }
-
-            // workaround for standing on dropoff edge
-            if (_charController.isGrounded)
-            {
-                if (Vector3.Dot(movement, _contact.normal) < 0)
-                {
-                    movement = _contact.normal * moveSpeed;
-                }
-                else
-                {
-                    movement += _contact.normal * moveSpeed;
-                }
-            }
-        }
-        movement.y = _vertSpeed;
-
-        movement *= Time.deltaTime;
-
-        if (playerCharacter.CanMove())
-        {
-            _charController.Move(movement);
+            _animator.SetBool("Kick", false);
         }
     }
 
