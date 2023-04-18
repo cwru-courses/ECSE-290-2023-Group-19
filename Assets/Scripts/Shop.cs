5028d5ac8814e830da277e2f19e24b143cebea5c
lsn19 <lsn19@case.edu>
2023-04-18 00:14:32 -0400

    updating shop and turrets

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,6 +4,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint cannon;
     public TurretBlueprint arrowTower;
+    public TurretBlueprint slowingTower;
     BuildManager buildManager;
 
    public void SelectCannon()
@@ -16,6 +17,12 @@ public class Shop : MonoBehaviour
     {
         Debug.Log("Arrow Turret Selected");
         buildManager.SelectTurretoBuild(arrowTower);
+    }
+
+    public void SelectSlowingTurret()
+    {
+        Debug.Log("Slowing Turret Selected");
+        buildManager.SelectTurretoBuild(slowingTower);
     }
 
     // Start is called before the first frame update
